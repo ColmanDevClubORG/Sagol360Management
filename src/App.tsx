69452004ca7b5f0dc9fac3d9ebe74529aef39a12
Tamir Abutbul <1tamir198@gmail.com>
2026-03-22 22:06:48 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,7 @@
 import './App.css'
-import { SGLButton } from './components/UI/Button/Button'
 
 function App() {
-  return (
-    <h1>
-      <SGLButton
-        text="Sagol360"
-        onClick={() => {
-          alert(123456)
-        }}
-        aria-label="Sagol360"
-        data-test-id="sagol360-button"
-        variant="contained"
-        style={{ color: 'red' }}
-      />
-
-      <SGLButton
-        text="12312"
-        onClick={() => {
-          alert(123456)
-        }}
-        variant="contained"
-      />
-
-      <SGLButton
-        text="423423"
-        onClick={() => {
-          alert(123456)
-        }}
-        aria-label="Sagol360"
-        data-test-id="sagol360-button"
-        variant="contained"
-      />
-    </h1>
-  )
+  return <h1>sagol360</h1>
 }
 
 export default App

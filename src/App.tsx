b04dfd09dff7e6e8a2300e0a94469d97d05f9341
Lavi2910 <almog.lavi29@gmail.com>
2026-03-24 00:55:29 +0200

    made the key {option}-{index}. made variant default value standard, options can get an array of string/number/react elment/mixed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { SGLButton } from './components/UI/Button/SGLButton'
+import { SGLSelect } from './components/UI/Select/SGLSelect'
 function App() {
   return (
     <>
       <SGLButton text="test" onClick={() => {}} />
       <SGLButton text="test" onClick={() => {}} />
+      <SGLSelect options={[1, 1, 'sda']}></SGLSelect>
     </>
   )
 }
--- src/components/UI/Select/SGLSelect.tsx
+++ src/components/UI/Select/SGLSelect.tsx
@@ -1,25 +1,32 @@
 import Select, { type SelectProps as MuiSelectProps } from '@mui/material/Select'
 import { selectStyles } from './styles'
 import { MenuItem } from '@mui/material'
+import { type ReactElement, isValidElement } from 'react'
+
+type SelectOption = string | number | ReactElement
 
 interface SelectProps extends Omit<MuiSelectProps, 'style'> {
-  options: string[]
+  options?: SelectOption[]
 }
 
-export const SGLSelect = ({ options, ...props }: SelectProps) => {
+export const SGLSelect = ({ options = [], variant = 'standard', ...props }: SelectProps) => {
   return (
     <Select
       sx={selectStyles}
-      variant="standard"
       disableUnderline
+      variant={variant}
       defaultValue={options[0]}
       {...props}
     >
-      {options.map((option) => (
-        <MenuItem key={option} value={option}>
-          {option}
-        </MenuItem>
-      ))}
+      {options.map((option, index) =>
+        isValidElement(option) ? (
+          option
+        ) : (
+          <MenuItem key={{ option } + '-' + { index }} value={option as string}>
+            {option}
+          </MenuItem>
+        ),
+      )}
     </Select>
   )
 }

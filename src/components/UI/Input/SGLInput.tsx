4672414c9848c1c4b293baffb875360dcd4085a6
Elad Abutbul <eladabutbul61@gmail.com>
2026-03-23 12:17:37 +0200

    add SGLInput generic component with custom styles

---
+++ src/components/UI/Input/SGLInput.tsx
@@ -0,0 +1,35 @@
+import TextField, { type TextFieldProps } from '@mui/material/TextField'
+import { type SxProps, type Theme } from '@mui/material/styles'
+import { inputStyles } from './styles'
+
+interface InputProps extends Omit<TextFieldProps, 'style'> {
+  label: string
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  style?: SxProps<Theme>
+}
+
+export const SGLInput = ({
+  label,
+  value,
+  onChange,
+  style,
+  variant = 'outlined',
+  fullWidth = true,
+  ...props
+}: InputProps) => {
+  return (
+    <TextField
+      {...props}
+      label={label}
+      value={value}
+      onChange={onChange}
+      variant={variant}
+      fullWidth={fullWidth}
+      sx={{
+        ...inputStyles,
+        ...(style as object),
+      }}
+    />
+  )
+}
---
+++ src/components/UI/Input/styles.ts
@@ -0,0 +1,3 @@
+import { type SxProps, type Theme } from '@mui/material/styles'
+
+export const inputStyles: SxProps<Theme> = {}

19c5c7968baefaf36a7fc981c979716eb514710f
Rachel Shtern <Rachelishteren1@gmail.com>
2026-03-26 22:05:32 +0200

    feat: update SGLButton component

--- src/components/UI/Button/SGLButton.tsx
+++ src/components/UI/Button/SGLButton.tsx
@@ -2,10 +2,19 @@ import Button, { type ButtonProps as MuiButtonProps } from '@mui/material/Button
 import { buttonStyles } from './styles'
 import type { SxProps, Theme } from '@mui/material/styles'
 
-interface ButtonProps extends Omit<MuiButtonProps, 'onClick' | 'style'> {
+const variantcolor = {
+  white: 'background.paper',
+  black: 'black',
+  purple: 'purple.main',
+  orange: 'orange.main',
+} as const
+
+interface ButtonProps extends Omit<MuiButtonProps, 'onClick' | 'style' | 'color'> {
   text: string
   onClick: () => void
   style?: SxProps<Theme>
+  color?: 'white' | 'black' | 'purple' | 'orange'
+  textColor?: 'white' | 'black' | 'purple' | 'orange'
 }
 
 export const SGLButton = ({
@@ -13,10 +22,28 @@ export const SGLButton = ({
   onClick,
   style,
   variant = 'contained',
+  color = 'purple',
+  textColor = 'white',
   ...props
 }: ButtonProps) => {
   return (
-    <Button sx={{ ...buttonStyles, ...style }} variant={variant} onClick={onClick} {...props}>
+    <Button
+      sx={{
+        ...buttonStyles,
+        ...(variant === 'contained' && {
+          backgroundColor: variantcolor[color],
+          color: variantcolor[textColor],
+        }),
+        ...(variant === 'outlined' && {
+          borderColor: variantcolor[color],
+          color: variantcolor[textColor],
+        }),
+        ...style,
+      }}
+      variant={variant}
+      onClick={onClick}
+      {...props}
+    >
       {text}
     </Button>
   )

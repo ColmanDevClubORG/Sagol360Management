e14f00e6b8d123ce6ce7a58f98d75af2edb04bd6
Tamir Abutbul <1tamir198@gmail.com>
2026-03-28 19:23:20 +0300

    added different languages support (#20)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { I18nextProvider } from 'react-i18next'
 import './index.css'
 import App from './App.tsx'
+import i18n from './i18n'
 
 const textColors = {
   black: '#000000',
@@ -71,7 +73,9 @@ const theme = createTheme({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
     </ThemeProvider>
   </StrictMode>,
 )

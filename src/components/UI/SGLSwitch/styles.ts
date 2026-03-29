0eb67b26450ba28f068e8657a3db53b92ab15991
beny25585 <beny25585@users.noreply.github.com>
2026-03-29 22:31:47 +0300

    fit: clean up and provide working badge component

---
+++ src/components/UI/SGLSwitch/styles.ts
@@ -0,0 +1,30 @@
+import type { Theme } from '@mui/material/styles';
+
+export const getSwitchStyles = (theme: Theme) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.primary.main,
+        opacity: 1,
+        border: 0,
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    width: 22,
+    height: 22,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 13,
+    backgroundColor: theme.palette.grey[300],
+    opacity: 1,
+  },
+});

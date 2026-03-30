a918ccbf1136c3d38aa7d3ff103d092dee860c6c
Lavi2910 <almog.lavi29@gmail.com>
2026-03-31 01:14:04 +0300

    added pressed logic for mobile aswell

--- src/components/NavBar/MobileNavBar.tsx
+++ src/components/NavBar/MobileNavBar.tsx
@@ -2,6 +2,7 @@ import { House, Calendar, Activity, ChartColumn } from 'lucide-react'
 import * as styles from './styles'
 import { theme } from '../../theme'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 const navLinks = [
   { id: 'home', href: '/home', icon: House, key: 'nav.home' },
@@ -11,13 +12,17 @@ const navLinks = [
 ]
 
 export const MobileNavBar = () => {
+  const location = useLocation()
   return (
     <nav style={styles.mobileNav}>
-      {navLinks.map(({ id, href, icon: Icon }) => (
-        <Link key={id} to={href}>
-          <Icon color={theme.palette.customGrey.main} />
-        </Link>
-      ))}
+      {navLinks.map(({ id, href, icon: Icon }) => {
+        const isActive = location.pathname === href
+        return (
+          <Link key={id} to={href}>
+            <Icon color={isActive ? theme.palette.purple.main : theme.palette.customGrey.main} />
+          </Link>
+        )
+      })}
     </nav>
   )
 }

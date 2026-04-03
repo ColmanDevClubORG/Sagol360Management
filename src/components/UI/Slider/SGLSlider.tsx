94bbebcbc7b24e3bfd2da2f28cd342fc91a69f96
Rachel Shtern <Rachelishteren1@gmail.com>
2026-04-03 13:12:17 +0300

    fix -style object and onChange function

--- src/components/UI/Slider/SGLSlider.tsx
+++ src/components/UI/Slider/SGLSlider.tsx
@@ -1,16 +1,18 @@
 import { Slider } from '@mui/material'
 import { sliderStyles } from './styles'
+import type { CSSProperties } from 'react'
 import { type SliderProps as MuiSliderProps } from '@mui/material'
 
 interface sliderProps extends Omit<MuiSliderProps, 'onChange'> {
-  onChange: (event: Event, value: number) => void
+  onChange: (value: number) => void
+  style?: CSSProperties
 }
 
-export const SGLSlider = ({ onChange, ...props }: sliderProps) => {
+export const SGLSlider = ({ onChange, style, ...props }: sliderProps) => {
   return (
     <Slider
-      sx={sliderStyles}
-      onChange={(event, value) => onChange(event, value as number)}
+      sx={{ ...sliderStyles, ...style }}
+      onChange={(_, value) => onChange(value as number)}
       {...props}
     />
   )

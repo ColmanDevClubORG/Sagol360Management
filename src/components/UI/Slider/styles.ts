70c1188e06c987e76843828f6ff2c60bd0531fdc
Rachel Shtern <Rachelishteren1@gmail.com>
2026-04-03 13:12:17 +0300

    fix -style object and onChange function

--- src/components/UI/Slider/styles.ts
+++ src/components/UI/Slider/styles.ts
@@ -4,8 +4,6 @@ export const sliderStyles = (theme: Theme) => ({
   '& .MuiSlider-thumb': {
     backgroundColor: theme.palette.purple.main,
     border: `2.5px solid ${theme.palette.background.paper}`,
-
-    ///boxShadow: `0 1px 1px ${theme.palette.gray.main}`,
     boxShadow: theme.shadows[1],
   },
   '& .MuiSlider-track': {

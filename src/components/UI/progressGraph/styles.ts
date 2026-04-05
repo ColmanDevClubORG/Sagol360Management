d6779ff7066c5e3a12ff8bc72a800ae07b9f8afc
Tamir Abutbul <1tamir198@gmail.com>
2026-04-05 16:52:56 +0300

    Add slot prop to the graph

--- src/components/UI/progressGraph/styles.ts
+++ src/components/UI/progressGraph/styles.ts
@@ -18,14 +18,13 @@ export const progressGraphStyles = {
   '& .MuiChartsLegend-series': { flexDirection: 'row-reverse' },
   '& .MuiChartsLegend-label': { fontSize: '0.9rem', color: 'midGrey.main' },
   '& .MuiLineElement-root': { strokeWidth: '0.25rem' },
-  '& .MuiChartsAxis-tickLabel': {
-    color: 'midGrey.main',
-    fill: 'currentColor !important',
-  },
 }
 
 export const progressGraphSlotProps: LineChartProps['slotProps'] = {
   legend: {
     position: { vertical: 'bottom' },
+  },
+  axisTickLabel: {
+    style: { fill: 'midGrey' },
   },
 }

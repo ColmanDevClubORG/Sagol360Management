dabcd2c6603672e4d33be58ad947114bd1c6c83a
Almog Lavi <almog.lavi29@gmail.com>
2026-04-15 19:50:20 +0300

    created login oprions (#50)
    * created login oprions
    
    * created protected routes and fixed resposive issues
    
    * Feat/add badge and switch (#46)
    
    * fit: clean up and provide working badge component
    
    * feat: Badge component ready
    
    * fix: simplify structure and resolve bot reviews
    
    * feat: cleanup and final sync
    
    * feat: cleanup and final sync
    
    * feat: cleanup and final sync
    
    * feat: cleanup and final sync
    
    * feat: cleanup and final sync
    
    * feat: cleanup and final sync
    
    * feat: cleanup and final sync
    
    * feat: cleanup and final sync
    
    * fix: logic with nullish coalescing and theme-based styling
    
    * fix: refactor Badge and Switch to use SGLTypography and theme colors
    
    * feat: implement SGLSwitch and Badge fixes according to PR reviews
    
    * feat: implement SGLSwitch and Badge fixes according to PR reviews
    
    * feat: implement SGLSwitch and Badge fixes according to PR reviews
    
    * feat: implement SGLSwitch and Badge fixes according to PR reviews
    
    ---------
    
    Co-authored-by: beny25585 <beny25585@users.noreply.github.com>
    Co-authored-by: Tamir Abutbul <1tamir198@gmail.com>
    
    * login is the main path and uses /login
    
    ---------
    
    Co-authored-by: OrtalNisim <165634151+OrtalNisim@users.noreply.github.com>
    Co-authored-by: beny25585 <beny25585@users.noreply.github.com>
    Co-authored-by: Tamir Abutbul <1tamir198@gmail.com>

---
+++ src/components/UI/Icons/Badge/styles.ts
@@ -0,0 +1,17 @@
+export const bellStyle = {
+  color: 'purple.main',
+  cursor: 'pointer',
+}
+
+export const messageStyle = {
+  '& .MuiBadge-badge': {
+    backgroundColor: 'error.main',
+    color: 'common.white',
+  },
+  cursor: 'pointer',
+}
+
+export const anchorOrigin = {
+  vertical: 'top',
+  horizontal: 'left',
+} as const

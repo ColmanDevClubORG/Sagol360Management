956726824ecb9e1e92ee097c68e0bdc3b2ec3372
beny25585 <beny25585@users.noreply.github.com>
2026-04-17 18:28:55 +0300

    hotfix: use SGLTypography for badge count

--- src/components/UI/Icons/Badge/SGLBadge.tsx
+++ src/components/UI/Icons/Badge/SGLBadge.tsx
@@ -1,5 +1,7 @@
+import React from 'react'
 import Badge from '@mui/material/Badge'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import { SGLTypography } from '../../Typography/SGLTypography'
 import { bellStyle, anchorOrigin, messageStyle } from './styles'
 
 type SGLBadgeProps = {
@@ -8,7 +10,16 @@ type SGLBadgeProps = {
 
 export const SGLBadge = ({ count = 0 }: SGLBadgeProps) => {
   return (
-    <Badge badgeContent={count} sx={messageStyle} anchorOrigin={anchorOrigin}>
+    <Badge 
+      badgeContent={
+        <SGLTypography variant="smallText" color="common.white">
+          {count}
+        </SGLTypography>
+      } 
+      sx={messageStyle} 
+      anchorOrigin={anchorOrigin}
+      showZero={false}
+    >
       <NotificationsNoneIcon sx={bellStyle} />
     </Badge>
   )

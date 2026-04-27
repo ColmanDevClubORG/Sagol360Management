85bac128e84958059c63db919dfa228f118e7356
Lavi2910 <almog.lavi29@gmail.com>
2026-04-27 17:43:13 +0300

    image reviews

--- src/components/UI/Image/SGLImage.tsx
+++ src/components/UI/Image/SGLImage.tsx
@@ -2,18 +2,16 @@ import type { CSSProperties } from 'react'
 import { useTheme } from '@mui/material'
 import * as styles from './styles'
 
-interface SGLImageProps {
-  src?: string
-  alt?: string
+type SGLImageProps = {
   style?: CSSProperties
-}
+} & ({ src: string; alt?: string } | { src?: string; alt: string })
 
-export const SGLImage = ({ src, alt, style }: SGLImageProps) => {
+export const SGLImage = ({ src, alt = '', style }: SGLImageProps) => {
   const theme = useTheme()
 
   return src ? (
-    <img src={src} alt={alt} style={{ ...styles.image, ...style }} />
+    <img src={src} alt={alt} style={{ ...styles.imageStyles, ...style }} />
   ) : (
-    <div style={{ ...styles.placeholder(theme), ...style }} />
+    <div style={{ ...styles.placeholderStyles(theme), ...style }} />
   )
 }
--- src/components/UI/Image/styles.ts
+++ src/components/UI/Image/styles.ts
@@ -1,13 +1,13 @@
 import type { CSSProperties } from 'react'
 import type { Theme } from '@mui/material'
 
-export const image: CSSProperties = {
+export const imageStyles: CSSProperties = {
   width: '100%',
   height: '100%',
   objectFit: 'cover',
 }
 
-export const placeholder = (theme: Theme): CSSProperties => ({
+export const placeholderStyles = (theme: Theme): CSSProperties => ({
   width: '100%',
   height: '100%',
   backgroundColor: theme.palette.midGrey.main,

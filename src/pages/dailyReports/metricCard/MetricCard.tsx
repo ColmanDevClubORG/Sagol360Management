0e58f925ee671955e97bf7108f5c40ea88a7da8a
Rachel Shtern <Rachelishteren1@gmail.com>
2026-04-27 17:58:24 +0300

    feat - MetricCard component

---
+++ src/pages/dailyReports/metricCard/MetricCard.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { MetricCardHeader } from './metricCardHeader'
+import { MetricCardLabel } from './metricCardLabel'
+import { rootStyle } from './style'
+import { SGLSlider } from '@/components/UI/Slider/SGLSlider'
+
+interface MetricCardProps {
+  titleKey: string
+  icon: React.ReactNode
+  onChange: (value: number) => void
+  minLabelKey: string
+  maxLabelKey: string
+}
+
+export const MetricCard = ({
+  titleKey,
+  ///value,
+  icon,
+  onChange,
+  minLabelKey,
+  maxLabelKey,
+}: MetricCardProps) => {
+  const [sliderValue, setSliderValue] = useState(1)
+
+  const handleUpdate = (newValue: number) => {
+    onChange(newValue)
+    setSliderValue(newValue)
+  }
+
+  return (
+    <div className="metric-card" style={rootStyle}>
+      <MetricCardHeader titleKey={titleKey} icon={icon} value={sliderValue} />
+      <SGLSlider onChange={handleUpdate} value={sliderValue} min={1} max={10} />
+      <MetricCardLabel minLabelKey={minLabelKey} maxLabelKey={maxLabelKey} />
+    </div>
+  )
+}

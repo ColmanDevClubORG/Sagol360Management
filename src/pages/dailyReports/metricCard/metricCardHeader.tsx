bff13c7e48425401affa6b3e813f13853a04a5c1
Rachel Shtern <Rachelishteren1@gmail.com>
2026-04-28 19:20:18 +0300

    fix: Review changes, and adding control over icon color

--- src/pages/dailyReports/metricCard/metricCardHeader.tsx
+++ src/pages/dailyReports/metricCard/metricCardHeader.tsx
@@ -1,16 +1,17 @@
 import { useTranslation } from 'react-i18next'
 import { useTheme } from '@mui/material'
 import { SGLTypography } from '@/components/UI/Typography/SGLTypography'
-import { headerTitleStyle, headerValueStyle, cardHeaderStyle } from './style'
+import { IconStyle, headerTitleStyle, headerValueStyle, cardHeaderStyle } from './style'
 import { METRIC_MAX_VALUE } from '@/constants'
 
 interface CardHeaderProps {
   titleKey: string
   icon: React.ReactNode
   value: number
+  colorIcon: 'primary' | 'secondary' | 'error' | 'midGrey' | 'yellow' | 'green'
 }
 
-export const MetricCardHeader = ({ titleKey, icon, value }: CardHeaderProps) => {
+export const MetricCardHeader = ({ titleKey, icon, value, colorIcon }: CardHeaderProps) => {
   const { t } = useTranslation()
   const theme = useTheme()
 
@@ -25,7 +26,7 @@ export const MetricCardHeader = ({ titleKey, icon, value }: CardHeaderProps) => 
         <div>
           <SGLTypography variant="mediumTitle">{t(titleKey)}</SGLTypography>
         </div>
-        <div>{icon}</div>
+        <div style={IconStyle(theme, colorIcon)}>{icon}</div>
       </div>
     </div>
   )

5723c5426ee3c94396c1a000b49d64765c890d5d
Rachel Shtern <Rachelishteren1@gmail.com>
2026-05-03 18:52:46 +0300

    fix:conflicts changes

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,19 +4,24 @@ import { BottomSheetDialog } from '@/components/BottomSheetDialog/BottomSheetDia
 import type { Appointment } from '@/components/BottomSheetDialog/types'
 import { TrainingGoal } from '../lifeStyle/physioAndTrainingTab/TraningGoal'
 import { DailyTips } from './dailyTips/DailyTips'
-import { useTranslation } from 'react-i18next'
 import { CheckIn } from './checkIn/CheckIn'
+import { SGLVideoCard } from '@/components/UI/Video/SGLVideoCard'
+import { useTranslation } from 'react-i18next'
 
 export const Home = () => {
-  const { t } = useTranslation()
   const [selectedAppointment, setSelectedAppointment] = useState<Appointment | undefined>({
-    id: '1',
-    title: t('progress.treatment'),
-    time: '00:07',
-    durationMinutes: 120,
-    location: t('appointment.orangeCellArizon'),
-    type: 'hyperbaric_chamber',
+    appointmentId: '1',
+    patientId: 1622017,
+    time: '10:12:50',
+    date: '2026-05-15',
+    chamber: 'appointment.chamber.hyperbaric',
+    chairNumber: 9,
+    treatmentNumber: 2,
+    status: 'confirmed',
   })
+
+  const { t } = useTranslation()
+
   return (
     <>
       <ProgressCard value={10} />
@@ -28,6 +33,11 @@ export const Home = () => {
       <TrainingGoal />
       <DailyTips />
       <CheckIn onDone={() => {}} />
+      <SGLVideoCard
+        title={t('balanceTraining.title')}
+        description={t('balanceTraining.description')}
+        durationInMinutes={10}
+      />
     </>
   )
 }

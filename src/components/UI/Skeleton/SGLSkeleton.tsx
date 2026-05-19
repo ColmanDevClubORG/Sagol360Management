fd424d636ba99dd4dfab30ddb67ea885ea44c0d3
Rotem <rotemglot12@gmail.com>
2026-05-19 18:42:51 +0300

    Update skeleton component

--- src/components/UI/Skeleton/SGLSkeleton.tsx
+++ src/components/UI/Skeleton/SGLSkeleton.tsx
@@ -1,47 +1,29 @@
-import Skeleton, { type SkeletonProps as MuiSkeletonProps } from '@mui/material/Skeleton'
-import { type ReactNode } from 'react'
-import { type SxProps, type Theme } from '@mui/material/styles'
+import Skeleton, {
+  type SkeletonProps as MuiSkeletonProps,
+} from '@mui/material/Skeleton'
 
-interface SGLSkeletonProps extends Omit<MuiSkeletonProps, 'children'> {
-  children?: ReactNode
-  width?: number | string
-  height?: number | string
-  textSize?: number | string
+import type { SxProps, Theme } from '@mui/material/styles'
+
+interface SGLSkeletonProps
+  extends Omit<MuiSkeletonProps, 'sx'> {
   skeletonColor?: string
-  radius?: number | string
-  opacity?: number
   sx?: SxProps<Theme>
 }
 
 export const SGLSkeleton = ({
-  children,
-  variant = 'text',
-  animation = 'wave',
-  width,
-  height,
-  textSize = '1rem',
   skeletonColor = '#8F3DFF',
-  radius,
-  opacity = 1,
+  animation = 'wave',
   sx,
   ...props
 }: SGLSkeletonProps) => {
   return (
     <Skeleton
-      variant={variant}
-      animation={animation}
-      width={width}
-      height={height}
       sx={{
-        ...(variant === 'text' ? { fontSize: textSize } : {}),
         bgcolor: skeletonColor,
-        borderRadius: radius,
-        opacity,
         ...sx,
       }}
+      animation={animation}
       {...props}
-    >
-      {children}
-    </Skeleton>
+    />
   )
 }

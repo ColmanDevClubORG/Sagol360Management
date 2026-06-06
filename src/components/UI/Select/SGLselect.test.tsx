6a127349660255cb9f0b96222c4ac596caaab14e
Rotem <rotemglot12@gmail.com>
2026-06-06 16:15:38 +0300

    Revert "Fix SGLSelect tests"
    This reverts commit 50f9bf2f37bf7bfd83c1420482a784f399bc576b.

--- src/components/UI/Select/SGLselect.test.tsx
+++ src/components/UI/Select/SGLselect.test.tsx
@@ -1,8 +1,6 @@
 import { describe, it, expect, afterEach } from 'vitest'
 import '@testing-library/jest-dom/vitest'
 import { render, screen, fireEvent, cleanup } from '@testing-library/react'
-import { ThemeProvider } from '@mui/material/styles'
-import { theme } from '../../../theme'
 
 import { SGLSelect } from './SGLSelect'
 
@@ -14,11 +12,7 @@ describe('SGLSelect', () => {
   const options = ['Apple', 'Banana', 'Orange']
 
   it('should select the relevant item', () => {
-    render(
-      <ThemeProvider theme={theme}>
-        <SGLSelect options={options} />
-      </ThemeProvider>,
-    )
+    render(<SGLSelect options={options} />)
 
     const select = screen.getByRole('combobox')
 
@@ -28,14 +22,11 @@ describe('SGLSelect', () => {
     fireEvent.click(option)
 
     expect(select).toHaveTextContent('Banana')
-  })
+  })  
+  
 
   it('should not select any item when dropdown is opened and closed', () => {
-    render(
-      <ThemeProvider theme={theme}>
-        <SGLSelect options={options} value="" />
-      </ThemeProvider>,
-    )
+    render(<SGLSelect options={options} value="" />)
 
     const select = screen.getByRole('combobox')
 
@@ -46,26 +37,22 @@ describe('SGLSelect', () => {
   })
 
   it('should change the selected value when selecting a different option', () => {
-    render(
-      <ThemeProvider theme={theme}>
-        <SGLSelect options={options} />
-      </ThemeProvider>,
-    )
+  render(<SGLSelect options={options} />)
 
-    const select = screen.getByRole('combobox')
+  const select = screen.getByRole('combobox')
 
-    fireEvent.mouseDown(select)
+  fireEvent.mouseDown(select)
 
-    const bananaOption = screen.getByText('Banana')
-    fireEvent.click(bananaOption)
+  const bananaOption = screen.getByText('Banana')
+  fireEvent.click(bananaOption)
 
-    expect(select).toHaveTextContent('Banana')
+  expect(select).toHaveTextContent('Banana')
 
-    fireEvent.mouseDown(select)
+  fireEvent.mouseDown(select)
 
-    const orangeOption = screen.getByText('Orange')
-    fireEvent.click(orangeOption)
+  const orangeOption = screen.getByText('Orange')
+  fireEvent.click(orangeOption)
 
-    expect(select).toHaveTextContent('Orange')
-  })
+  expect(select).toHaveTextContent('Orange')
+})
 })

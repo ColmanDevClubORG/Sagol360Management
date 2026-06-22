56b841d288c8b6c5953ff305af9708d0c993305f
Rachel Shtern <Rachelishteren1@gmail.com>
2026-06-23 00:58:20 +0300

    fix

--- src/pages/home/AppointmentBottomSheet/AppointmentBottomSheet.tsx
+++ src/pages/home/AppointmentBottomSheet/AppointmentBottomSheet.tsx
@@ -4,19 +4,20 @@ import { apiService } from '@/services/api/api.service'
 import { useState } from 'react'
 import type { Appointment } from '@/components/BottomSheetDialog/types'
 import { API_ENDPOINTS } from '@/constants/api.constants'
+import { MOCK_DATA } from '@/constants'
 
 export const AppointmentBottomSheet = () => {
   const [selectedAppointment, setSelectedAppointment] = useState<boolean>(true)
 
-  const patientID = '1622017'
-  const date = '07-06-2026' // Mock data
+  const patientid = MOCK_DATA.patientID
+  const date = MOCK_DATA.date
 
   const { data: appointment } = useQuery({
-    queryKey: ['nextAppointment', patientID, date],
+    queryKey: ['nextAppointment', patientid, date],
     queryFn: async () => {
       const response = await apiService.get<Appointment>(API_ENDPOINTS.appointment, {
         params: {
-          patientID,
+          patientid,
           date,
         },
       })

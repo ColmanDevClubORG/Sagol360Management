f5e3cc60fa7f16d6dbfc43ab421a9bb6942a115d
Racheli Shteren <rachelishteren1@gmail.com>
2026-06-25 00:41:45 +0300

    feat: 86extdm6t (#89)
    * feat: add conected to back
    
    * fix: add Appointment component
    
    * fix

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -2,5 +2,6 @@ export const API_ENDPOINTS = {
   patient: '/api/patients/',
   metrics: '/api/metrics/',
   metricsToday: '/api/metrics/today/',
+  appointment: '/api/appointment/nextAppointment',
   email: '/api/email',
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,12 @@
+import { formatIsoDate } from '@/utils/datesUtils'
+
 export const DOT = '•'
 export const METRIC_MAX_VALUE = '10'
 export const SLIDER_MIN_VALUE = 1
 export const SLIDER_MAX_VALUE = 10
 export const BALANCE_VIDEO_TIME = 15
 export const STRETCH_VIDEO_TIME = 10
+export const MOCK_DATA = {
+  patientID: Number('1622017'),
+  date: formatIsoDate(new Date(2026, 5, 23)),
+}
